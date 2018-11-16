7292e9ba90f07beeaa63260c8229e0f074dfd648
Dean Keithly <drk94@cornell.edu>
2018-11-15 22:23:27 -0500

    adding nuclear option where we purge_everything from rc job and fresh restart

--- EXOSIMS/SurveyEnsemble/IPClusterEnsemble.py
+++ EXOSIMS/SurveyEnsemble/IPClusterEnsemble.py
@@ -60,50 +60,60 @@ class IPClusterEnsemble(SurveyEnsemble):
             sim:
 
         """
+        restartRuns = True # a boolean to determine whether there are runs left to run or not
+        numRunStarts = 1
+        while restartRuns and nb_run_sim > 0: # will  continually try to entirely restart the remaining runs
+            restartRuns = False
+            print('Starting Runs for the ' + str(numRunStarts) + ' time')
 
-        t1 = time.time()
-        async_res = []
-        for j in range(nb_run_sim):
-            ar = self.lview.apply_async(run_one, genNewPlanets=genNewPlanets,
-                    rewindPlanets=rewindPlanets, **kwargs)
-            async_res.append(ar)
-        
-        print("Submitted %d tasks."%len(async_res))
-        
-        runStartTime = time.time()#create job starting time
-        avg_time_per_run = 0.
-        tmplenoutstandingset = nb_run_sim
-        tLastRunFinished = time.time()
-        ar= self.rc._asyncresult_from_jobs(async_res)
-        while not ar.ready():
-            ar.wait(10.)
-            clear_output(wait=True)
-            if ar.progress > 0:
-                timeleft = ar.elapsed/ar.progress * (nb_run_sim - ar.progress)
-                if timeleft > 3600.:
-                    timeleftstr = "%2.2f hours"%(timeleft/3600.)
-                elif timeleft > 60.:
-                    timeleftstr = "%2.2f minutes"%(timeleft/60.)
+            t1 = time.time()
+            async_res = []
+            for j in range(nb_run_sim):
+                ar = self.lview.apply_async(run_one, genNewPlanets=genNewPlanets,
+                        rewindPlanets=rewindPlanets, **kwargs)
+                async_res.append(ar)
+            
+            print("Submitted %d tasks."%len(async_res))
+            
+
+            runStartTime = time.time()#create job starting time
+            avg_time_per_run = 0.
+            tmplenoutstandingset = nb_run_sim
+            tLastRunFinished = time.time()
+            ar= self.rc._asyncresult_from_jobs(async_res)
+            while not ar.ready():
+                ar.wait(10.)
+                clear_output(wait=True)
+                if ar.progress > 0:
+                    timeleft = ar.elapsed/ar.progress * (nb_run_sim - ar.progress)
+                    if timeleft > 3600.:
+                        timeleftstr = "%2.2f hours"%(timeleft/3600.)
+                    elif timeleft > 60.:
+                        timeleftstr = "%2.2f minutes"%(timeleft/60.)
+                    else:
+                        timeleftstr = "%2.2f seconds"%timeleft
                 else:
-                    timeleftstr = "%2.2f seconds"%timeleft
-            else:
-                timeleftstr = "who knows"
+                    timeleftstr = "who knows"
 
-            #Terminate hanging runs
-            outstandingset = self.rc.outstanding#a set of msg_ids that have been submitted but resunts have not been received
-            if len(outstandingset) > 0 and len(outstandingset) < nb_run_sim:#there is at least 1 run still going and we have not just started
-                avg_time_per_run = (time.time() - runStartTime)/float(nb_run_sim - len(outstandingset))#compute average amount of time per run
-                if len(outstandingset) < tmplenoutstandingset:#The scheduler has finished a run
-                    tmplenoutstandingset = len(outstandingset)#update this. should decrease by ~1 or number of cores...
-                    tLastRunFinished = time.time()#update tLastRunFinished to the last time a simulation finished (right now)
-                    #self.vprint("tmplenoutstandingset %d, tLastRunFinished %0.6f"%(tmplenoutstandingset,tLastRunFinished))
-                if time.time() - tLastRunFinished > avg_time_per_run*(1 + self.maxNumEngines*2):
-                    self.vprint('Aborting ' + str(len(self.rc.outstanding)) + 'qty outstandingset jobs')
-                    self.rc.abort()#by default should abort all outstanding jobs... #it is possible that this will not stop the jobs running
-                    tLastRunFinished = time.time()#update tLastRunFinished to the last time a simulation was restarted (right now)
+                #Terminate hanging runs
+                outstandingset = self.rc.outstanding#a set of msg_ids that have been submitted but resunts have not been received
+                if len(outstandingset) > 0 and len(outstandingset) < nb_run_sim:#there is at least 1 run still going and we have not just started
+                    avg_time_per_run = (time.time() - runStartTime)/float(nb_run_sim - len(outstandingset))#compute average amount of time per run
+                    if len(outstandingset) < tmplenoutstandingset:#The scheduler has finished a run
+                        tmplenoutstandingset = len(outstandingset)#update this. should decrease by ~1 or number of cores...
+                        tLastRunFinished = time.time()#update tLastRunFinished to the last time a simulation finished (right now)
+                        #self.vprint("tmplenoutstandingset %d, tLastRunFinished %0.6f"%(tmplenoutstandingset,tLastRunFinished))
+                    if time.time() - tLastRunFinished > avg_time_per_run*(1 + self.maxNumEngines*2):
+                        nb_run_sim = len(self.rc.outstanding)
+                        restartRuns = True
+                        self.vprint('Aborting ' + str(len(self.rc.outstanding)) + 'qty outstandingset jobs')
+                        self.rc.abort()#by default should abort all outstanding jobs... #it is possible that this will not stop the jobs running
+                        self.rc.purge_everything() # purge all results if outstanding *because rc.abort() didn't seem to do the job right
+                        tLastRunFinished = time.time()#update tLastRunFinished to the last time a simulation was restarted (right now)
 
-            print("%4i/%i tasks finished after %4i s. About %s to go." % (ar.progress, nb_run_sim, ar.elapsed, timeleftstr), end="")
-            sys.stdout.flush()
+                print("%4i/%i tasks finished after %4i s. About %s to go." % (ar.progress, nb_run_sim, ar.elapsed, timeleftstr), end="")
+                sys.stdout.flush()
+            numRunStarts += 1 # increment number of run restarts
 
         t2 = time.time()
         print("\nCompleted in %d sec" % (t2 - t1))
