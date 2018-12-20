re-adding fZ calculation prior to target selection in prioirty obs but with try except FIXING return INDENT

EXOSIMS/SurveySimulation/SLSQPScheduler.py
@@ -478,9 +478,8 @@ class SLSQPScheduler(SurveySimulation):
                 fZ = self.ZodiacalLight.fZ(self.Observatory, self.TargetList, sInds,  
                     self.TimeKeeping.currentTimeAbs.copy() + slewTimes[sInds], self.detmode)
             except:
-                    self.vprint('Calculate fZ failed mission is over?')
-                    return None, None
-                return
+                self.vprint('Calculate fZ failed mission is over?')
+                return None, None
             selectInd = np.argmin(np.abs(fZ - valfZmin))#this is most negative when fZ is smallest 
             sInd = sInds[selectInd]
 