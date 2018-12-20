2edb1a2712ed868e2fe1d14e1817f8a3d9f80e63
Dean Keithly <drk94@cornell.edu>
2018-12-19 23:32:58 -0500

    suppressing fZrecalculation

--- EXOSIMS/SurveySimulation/SLSQPScheduler.py
+++ EXOSIMS/SurveySimulation/SLSQPScheduler.py
@@ -474,8 +474,8 @@ class SLSQPScheduler(SurveySimulation):
             #self.vprint('advancedToAbsTime: ' + str(TK.currentTimeAbs.copy()))
             waitTime = None
 
-            fZ = self.ZodiacalLight.fZ(self.Observatory, self.TargetList, sInds,  
-                self.TimeKeeping.currentTimeAbs.copy() + slewTimes[sInds], self.detmode)
+            #fZ = self.ZodiacalLight.fZ(self.Observatory, self.TargetList, sInds,  
+            #    self.TimeKeeping.currentTimeAbs.copy() + slewTimes[sInds], self.detmode)
             selectInd = np.argmin(np.abs(fZ - valfZmin))#this is most negative when fZ is smallest 
             sInd = sInds[selectInd]
 
